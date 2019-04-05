e14740ee901a81dbf690b21d46081ce998abb752
Sam Duan <dsqchn@gmail.com>
2019-04-05 09:26:30 -0400

    msa version

--- src/RandomRobot.java
+++ src/RandomRobot.java
@@ -1,4 +1,26 @@
+/**
+ * RandomRobot is a subclass of Robot.
+ * @author
+ */
+public class RandomRobot extends Robot
+{
+    // instance fields here
 
-public class RandomRobot extends Robot{
-
+    /**
+     * Creates a new RandomRobot with the given maze.
+     * @param theMaze the maze to traverse 
+     * @pre theMaze is not null
+     */
+    public RandomRobot(Maze theMaze)
+    {
+        super(theMaze);
+    }
+      
+    /**
+     * Moves the robot one step.
+     */
+    public void move()
+    {
+        
+    }
 }
---
+++ src/RightHandRuleRobot.java
@@ -0,0 +1,26 @@
+/**
+ * RightHandRuleRobot is a subclass of Robot.
+ * @author
+ */
+public class RightHandRuleRobot extends Robot
+{
+    // instance fields here
+
+    /**
+     * Creates a new RightHandRobot with the given maze
+     * @param theMaze the maze to traverse
+     * @pre theMaze is not null
+     */
+    public RightHandRuleRobot(Maze theMaze)
+    {
+        super(theMaze);
+    }
+
+    /**
+     * Moves the robot one step.
+     */
+    public void move()
+    {
+        
+    }
+}
